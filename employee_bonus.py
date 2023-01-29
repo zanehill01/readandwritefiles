f13bda171ee464bdf609f8f4aa3aa02fe1312f6d
zanehill01 <zane_hill1@baylor.edu>
2023-01-28 19:52:21 -0600

    employee  bonus done

--- employee_bonus.py
+++ employee_bonus.py
@@ -1,1 +1,17 @@
 # program that reads the EmployeePay.csv file and prints out details of each employee
+# ID,EmpFName,EmpLName,Salary,Bonus
+
+
+import csv
+EMPLOYEE = 'employeepay.csv'
+
+#
+
+infile = open(EMPLOYEE, 'r', newline='')
+reader = csv.reader(infile)
+
+#
+
+for row in reader:
+
+    print(row)
