579dd5a71d243a1203b72af1fc80bb0777930920
zanehill01 <zane_hill1@baylor.edu>
2023-01-30 20:40:51 -0600

    fixed employee bonus output

--- employee_bonus.py
+++ employee_bonus.py
@@ -17,5 +17,10 @@ for row in reader:
     bonus = int(row[3]) * float(row[4])
     total = bonus + int(row[3])
 
-    print([row, 'TOTAL PAY -> ', format(total, '.2f')])
-    input('Press ENTER for next employee...')
+    print('ID EmpFName     EmpLName     Salary     Bonus     Total Pay')
+    print('-- ------------ ------------ ---------- --------- ---------')
+
+    print(format(row[0], '<2'), format(row[1], '<12'), format(row[2], '<12'),
+          format(row[3], '<10'), format(row[4], '<9'), format(total, '.2f'))
+
+    input('\nPress ENTER for next employee...\n')
